143401d9a48ff2cfa056b27dc137ee187be603c2
fate.chang <fate.chang@cerence.com>
2021-06-10 22:23:46 +0800

    Avoid crash when current connected device disconnect after activity is closed

--- android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutter_blue/FlutterBluePlugin.java
@@ -1010,7 +1010,9 @@ public class FlutterBluePlugin implements FlutterPlugin, ActivityAware, MethodCa
                 new Runnable() {
                     @Override
                     public void run() {
-                        channel.invokeMethod(name, byteArray);
+                        if (channel != null) {
+                            channel.invokeMethod(name, byteArray);
+                        }
                     }
                 });
     }
