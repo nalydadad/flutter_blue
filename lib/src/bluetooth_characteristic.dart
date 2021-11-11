5460a575def0c7b1b9943b93f9e5607bd0181202
Felix.lin <fly_81211@hotmail.com>
2021-11-11 14:56:50 +0800

    Remove unused code

--- lib/src/bluetooth_characteristic.dart
+++ lib/src/bluetooth_characteristic.dart
@@ -26,7 +26,7 @@ class BluetoothCharacteristic {
         _onValueChangedStream,
       ]);
 
-  List<int> get lastValue => _value.value ?? [];
+  List<int> get lastValue => _value.value;
 
   BluetoothCharacteristic.fromProto(protos.BluetoothCharacteristic p)
       : uuid = new Guid(p.uuid),
